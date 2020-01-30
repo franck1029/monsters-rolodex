3d62579424b3ba22eb0d8dacac634a06a16720fc
franckle <franckle.celestial@gmail.com>
2020-01-30 13:24:30 +0800

    Updates

---
+++ precache-manifest.44a5fd9c4a2be0e52611d878f8572b5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11a63eaeb796b1ac7482606012721db5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "95c962aca06f6ddc2134",
+    "url": "/monsters-rolodex/static/css/main.5c842c4d.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "95c962aca06f6ddc2134",
+    "url": "/monsters-rolodex/static/js/main.7a4f9c00.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
